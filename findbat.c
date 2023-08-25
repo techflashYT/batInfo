1f98c1d34bb13897938482acfd3e8457062a571a
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-08-25 08:42:00 -0700

    Make sure that it's *actually* a battery

--- findbat.c
+++ findbat.c
@@ -3,6 +3,8 @@
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+
 
 int main() {
 	DIR *dir;
@@ -17,12 +19,24 @@ int main() {
 
 	while ((entry = readdir(dir)) != NULL) {
 		if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-			snprintf(path, sizeof(path), "/sys/class/power_supply/%s/capacity", entry->d_name);
+			snprintf(path, sizeof(path), "/sys/class/power_supply/%s/type", entry->d_name);
 			if (access(path, F_OK) == 0) {
-				// we found one with capacity
-				printf("/sys/class/power_supply/%s", entry->d_name);
-				closedir(dir);
-				exit(0);
+				// we found one with type
+				int fd = open(path, O_RDONLY);
+				if (fd < 0) {
+					perror("ERROR: Failed to open type");
+					exit(1);
+				}
+				char type[256];
+				if (read(fd, type, sizeof(type)) < 0) {
+					perror("ERROR: Failed to read type");
+					exit(1);
+				}
+				close(fd);
+
+				if (strcmp(type, "Battery\n") == 0) {
+					goto goodbat;
+				}
 			}
 		}
 	}
@@ -30,4 +44,9 @@ int main() {
 	closedir(dir);
 	fprintf(stderr, "FINDBAT-ERROR: No battery found!!!\n");
 	exit(1);
+
+	goodbat:
+	printf("/sys/class/power_supply/%s", entry->d_name);
+	closedir(dir);
+	return 0;
 }
